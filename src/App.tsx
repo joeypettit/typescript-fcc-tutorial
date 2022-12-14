bf331c85b326f7a792b0b7463be389230aaac97e
Joey Pettit <pettitbass@gmail.com>
2022-12-13 19:18:30 -0600

    finished through 17:23

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+let name: string;
+let age: number | string;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+
+let pringName: (name: string) => never;
+
+let personName: unknown;
+
+name = "joey";
+
+// type Person = {
+//   name: string;
+//   age?: number;
+// };'
+
+type X = {
+  a: string;
+  b: number;
+};
+
+interface Person extends X {
+  name: string;
+  age?: number;
+}
+
+interface Guy extends Person {
+  profession: string;
+}
+
+type Y = X & {
+  c: string;
+  d: number;
+};
+
+// let person: Person = {
+//   name: "joey",
+// };
+
+// let lotsOfPeople: Person[];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">Hello </div>;
 }
 
 export default App;
